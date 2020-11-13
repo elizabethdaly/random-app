3c35ea48292f15236749da2d783a0147916302c7
elizabethdaly <liz.m.a.daly@gmail.com>
2020-11-13 17:18:07 +0000

    Clone IMcL repo and follow along with web server lectures

--- rando.py
+++ rando.py
@@ -5,9 +5,25 @@ import flask as fl
 import numpy as np
 
 # Create a new web app.
+# Calling Flask function in flask package
+# __name__ : built in var in Python
 app = fl.Flask(__name__)
 
 # Add root route.
 @app.route('/')
-def stdnor():
-  return {"value": np.random.standard_normal()}
+def home():
+  #return "Hello world."
+  return app.send_static_file('index.html')
+
+# tell Flask to make this fn available at /uniform
+# dynamic response
+@app.route('/api/uniform')
+def uniform():
+  return {"value": np.random.uniform()}
+
+# Add root route.
+# tell Flask to make this fn available at /normal
+# dynamic response
+@app.route('/api/normal')
+def normal():
+  return {"value": np.random.normal()}
